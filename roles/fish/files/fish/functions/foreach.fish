43732469b89b66cd1c5354df17fd18777955a14b
wintus <6386129+wintus@users.noreply.github.com>
2020-06-19 23:18:27 +0900

    Replace fish role

--- roles/fish/files/fish/functions/foreach.fish
+++
@@ -1,5 +0,0 @@
-function foreach --description='List array-type variable.'
-    for x in $argv
-        echo $x
-    end
-end
--- roles/fish/files/fish/functions/g.fish
+++
@@ -1,3 +0,0 @@
-function g --description='Go to git repo under control of GHQ.'
-    cd (ghq root)/(ghq list | peco)
-end
